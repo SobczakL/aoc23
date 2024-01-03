90e63d32bf3638a31dc1c9a91ff033cb5aa5d643
SobczakL <lucassobczak3@gmail.com>
2024-01-02 21:34:35 -0500

    second attempt

--- src/main.py
+++ src/main.py
@@ -1,37 +1,60 @@
 file_path = 'input.txt'
 
-with open(file_path, 'r') as file:
-    lines = file.readlines()
+text_to_num = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'ten': 10
+}
 
-    # seperate file into list
-    seperate_lines = []
-    for line in lines:
-        if line.endswith('\n'):
-            seperate_lines.append(line[:-2])
-
-    text_to_num = {
-        'zero': 0,
-        'one': 1,
-        'two': 2,
-        'three': 3,
-        'four': 4,
-        'five': 5,
-        'six': 6,
-        'seven': 7,
-        'eight': 8,
-        'nine': 9,
-        'ten': 10
-    }
-    total = 0
-
-    for line in seperate_lines:
-        
-        digits = [char for char in line if char.isdigit()]
-
-        if len(digits) >= 2:
-            first_last_combined = int(digits[0] + digits[-1])
-
-            total += first_last_combined
+def replace_number_with_word(string):
+    for word, number in text_to_num.items():
+        string = string.replace(str(number), word)
+    return string
 
 
-    print(total)
+def string_to_number(string):
+    substring_with_indices = []
+
+    for word in text_to_num.keys():
+        index = string.find(word)
+
+        while index != -1:
+            substring_with_indices.append((word, index))
+            index = string.find(word, index + 1)
+
+    substring_with_indices.sort(key=lambda x: x[1])
+
+    substrings_translated = [text_to_num[word] for word, _ in substring_with_indices]
+    return substrings_translated
+
+
+def solution():
+    second_sum_of_digit = 0
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+        for line in lines:
+            line = line.strip()
+
+            string = replace_number_with_word(line)
+            digit = string_to_number(string)
+
+            first_digit = digit[0]
+            last_digit = digit[-1]
+            final_digit = first_digit * 10 + last_digit
+
+            second_sum_of_digit += final_digit
+
+    return second_sum_of_digit
+
+
+solution()
